6109f104614be40fe85b59f3863bab32b424a3f2
Vinicius <vini.wessner@gmail.com>
2024-08-23 19:57:18 -0300

    created splash view

--- app/src/main/java/com/example/digitalbank/presenter/splash/SplashFragment.kt
+++ app/src/main/java/com/example/digitalbank/presenter/splash/SplashFragment.kt
@@ -5,17 +5,27 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.digitalbank.R
+import com.example.digitalbank.databinding.FragmentSplashBinding
 
 
 class SplashFragment : Fragment() {
 
+    private var _binding: FragmentSplashBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_splash, container, false)
+        _binding = FragmentSplashBinding.inflate(inflater, container, false)
+        return binding.root
     }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
 
 }
