29659677beee53db5aead417b7683685f56a037d
Vinicius <vini.wessner@gmail.com>
2024-08-24 16:26:20 -0300

    created functions validade infos recover account, login and register

--- app/src/main/java/com/example/digitalbank/presenter/auth/recover/RecoverFragment.kt
+++ app/src/main/java/com/example/digitalbank/presenter/auth/recover/RecoverFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import com.example.digitalbank.R
 import com.example.digitalbank.databinding.FragmentRecoverBinding
 
@@ -20,6 +21,27 @@ class RecoverFragment : Fragment() {
     ): View {
         _binding = FragmentRecoverBinding.inflate(inflater, container, false)
          return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initListeners()
+    }
+
+    private fun initListeners() {
+        binding.btnSendLink.setOnClickListener {
+            validadeData()
+        }
+    }
+
+    private fun validadeData() {
+        val email = binding.editEmail.text.toString().trim()
+
+        if (email.isNotEmpty()){
+            Toast.makeText(requireContext(), "Email enviado", Toast.LENGTH_SHORT).show()
+        }else{
+            Toast.makeText(requireContext(), "Digite seu e-mail correto", Toast.LENGTH_SHORT).show()
+        }
     }
 
     override fun onDestroyView() {
--- app/src/main/java/com/example/digitalbank/presenter/auth/register/RegisterFragment.kt
+++ app/src/main/java/com/example/digitalbank/presenter/auth/register/RegisterFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import com.example.digitalbank.R
 import com.example.digitalbank.databinding.FragmentRegisterBinding
 
@@ -22,6 +23,46 @@ class RegisterFragment : Fragment() {
     ): View? {
         _binding = FragmentRegisterBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initListeners()
+    }
+
+    private fun initListeners() {
+        binding.btnRegister.setOnClickListener {
+            validadeDate()
+        }
+    }
+
+    private fun validadeDate() {
+        val name = binding.editName.text.toString().trim()
+        val email = binding.editEmail.text.toString().trim()
+        val celular = binding.editPhone.text.toString().trim()
+        val senha = binding.editSenha.text.toString().trim()
+        val confirmSenha = binding.editConfirmSenha.text.toString().trim()
+
+
+        if (name.isNotEmpty()){
+            if (email.isNotEmpty()){
+                if (celular.isNotEmpty()){
+                    if (senha == confirmSenha && senha.isNotEmpty() && confirmSenha.isNotEmpty()){
+                        TODO("Fazer login")
+                        Toast.makeText(requireContext(), "Cadastro realizado", Toast.LENGTH_SHORT).show()
+                    } else {
+                        Toast.makeText(requireContext(), "Verifique sua senha", Toast.LENGTH_SHORT).show()
+                    }
+                }else{
+                    Toast.makeText(requireContext(), "Digite seu celular", Toast.LENGTH_SHORT).show()
+                }
+            }else{
+                Toast.makeText(requireContext(), "Digite uma senha", Toast.LENGTH_SHORT).show()
+            }
+        } else{
+            Toast.makeText(requireContext(), "Digite seu nome", Toast.LENGTH_SHORT).show()
+        }
+
     }
 
     override fun onDestroyView() {
