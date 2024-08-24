4907ac0c9c1a22d7462ea2ac8ecdf151240b2dc2
Vinicius <vini.wessner@gmail.com>
2024-08-24 13:35:28 -0300

    create screen register XML

---
+++ app/src/main/java/com/example/digitalbank/presenter/auth/register/RegisterFragment.kt
@@ -0,0 +1,33 @@
+package com.example.digitalbank.presenter.auth.register
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.digitalbank.R
+import com.example.digitalbank.databinding.FragmentRegisterBinding
+
+
+class RegisterFragment : Fragment() {
+
+
+    private var _binding: FragmentRegisterBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentRegisterBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+
+}
