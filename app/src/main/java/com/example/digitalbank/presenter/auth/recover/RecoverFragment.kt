e3e144db2343c5433058f389451422f19f274d97
Vinicius <vini.wessner@gmail.com>
2024-08-28 09:10:46 -0300

    created method reccover account

--- app/src/main/java/com/example/digitalbank/presenter/auth/recover/RecoverFragment.kt
+++ app/src/main/java/com/example/digitalbank/presenter/auth/recover/RecoverFragment.kt
@@ -6,8 +6,12 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.core.view.isVisible
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import com.example.digitalbank.R
 import com.example.digitalbank.databinding.FragmentRecoverBinding
+import com.example.digitalbank.util.StateView
 import com.example.digitalbank.util.initToolbar
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -16,6 +20,8 @@ class RecoverFragment : Fragment() {
 
     private var _binding: FragmentRecoverBinding? = null
     private val binding get() = _binding!!
+
+    private val recoverViewModel: RecoverViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -42,9 +48,29 @@ class RecoverFragment : Fragment() {
         val email = binding.editEmail.text.toString().trim()
 
         if (email.isNotEmpty()){
-            Toast.makeText(requireContext(), "Email enviado", Toast.LENGTH_SHORT).show()
+            recouverAccount(email)
         }else{
             Toast.makeText(requireContext(), "Digite seu e-mail correto", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun recouverAccount(email: String){
+
+        recoverViewModel.recover(email).observe(viewLifecycleOwner){ stateView ->
+            when(stateView){
+                is StateView.Loading -> {
+                    binding.progressBar.isVisible = true
+                }
+                is StateView.Sucess -> {
+                    binding.progressBar.isVisible = false
+                    Toast.makeText(requireContext(), "Verifique sua caixa de SPAM", Toast.LENGTH_SHORT).show()
+                    findNavController().popBackStack()
+                }
+                is StateView.Error -> {
+                    binding.progressBar.isVisible = false
+                    Toast.makeText(requireContext(), stateView.message, Toast.LENGTH_SHORT).show()
+                }
+            }
         }
     }
 
