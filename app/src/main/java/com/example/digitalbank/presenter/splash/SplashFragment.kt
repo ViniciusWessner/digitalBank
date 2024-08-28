2f8855bc1c7b94bc7c076d70b5587f34e548e6a8
Vinicius <vini.wessner@gmail.com>
2024-08-27 22:43:56 -0300

    created firebasehelper and configure delay in inicialization splash fragment

--- app/src/main/java/com/example/digitalbank/presenter/splash/SplashFragment.kt
+++ app/src/main/java/com/example/digitalbank/presenter/splash/SplashFragment.kt
@@ -1,11 +1,16 @@
 package com.example.digitalbank.presenter.splash
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.example.digitalbank.R
 import com.example.digitalbank.databinding.FragmentSplashBinding
+import com.example.digitalbank.util.FirebaseHelper
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -18,9 +23,22 @@ class SplashFragment : Fragment() {
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         _binding = FragmentSplashBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        Handler(Looper.getMainLooper()).postDelayed(this::verifyAuth, 3000) //3s para chamar a funcao verifyAuth()
+    }
+
+    private fun verifyAuth(){
+        if (FirebaseHelper.isAuthenticated()){
+            findNavController().navigate(R.id.action_splashFragment_to_homeFragment)
+        }else {
+            findNavController().navigate(R.id.action_splashFragment_to_navigation)
+        }
     }
 
     override fun onDestroyView() {
